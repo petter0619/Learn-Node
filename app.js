57ac20c534413a6a4cb0b5d291eb387f69143ac3
Petter Carlsson <pettercarlsson@Petters-iMac.local>
2020-09-16 14:04:30 +0200

    File changes to enable start

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Exposes a bunch of methods for validating data. Used heavily on userController.validateRegister
-app.use(expressValidator());
+//app.use(expressValidator());
 
 // populates req.cookies with any cookies that came along with the request
 app.use(cookieParser());
@@ -60,10 +60,10 @@ app.use((req, res, next) => {
 });
 
 // promisify some callback based APIs
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   req.login = promisify(req.login, req);
   next();
-});
+});*/
 
 // After allllll that above middleware, we finally handle our own routes!
 app.use('/', routes);
