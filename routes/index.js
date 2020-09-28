9e7f7232619618e399f2f2d29bce418ac969e9c7
Petter Carlsson <pettercarlsson@Petters-iMac.local>
2020-09-28 14:42:14 +0200

    Save new store in MongoDB added

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 // Import route handlers
 const storeController = require('../controllers/storeController');
+// Import error handler
+const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
 router.get('/', storeController.homePage);
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
