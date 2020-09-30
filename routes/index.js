3d6dfc9be3935177402622c834b12cb3938136bf
Petter Carlsson <pettercarlsson@Petters-iMac.local>
2020-09-30 17:56:33 +0200

    Login && Register Forms ++ Validation middleware

--- routes/index.js
+++ routes/index.js
@@ -3,28 +3,41 @@ const router = express.Router();
 
 // Import route handlers
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 // Import error handler
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
 router.get('/', catchErrors(storeController.getStores));
+
 router.get('/stores', catchErrors(storeController.getStores));
+
 router.get('/add', storeController.addStore);
+
 router.post('/add', 
     storeController.upload,
     catchErrors(storeController.resize),
     catchErrors(storeController.createStore)
 );
+
 router.post('/add/:id', 
     storeController.upload,
     catchErrors(storeController.resize),
     catchErrors(storeController.updateStore)
 );
+
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
+
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
+//router.post('/login');
+
+router.get('/register', userController.registerForm);
+router.post('/register', userController.validateRegister);
 
 module.exports = router;
