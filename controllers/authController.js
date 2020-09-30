49dd893725e8045cc31a980e6a73cf56ee22a5da
Petter Carlsson <pettercarlsson@Petters-iMac.local>
2020-09-30 18:57:41 +0200

    Added isLoggedIn middleware auth for /Add page

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,14 +2,25 @@ const passport = require('passport');
 
 
 exports.login = passport.authenticate('local', {
-    failureRedirect: '/login',
     failureFlash: 'Failed login!',
+    failureRedirect: '/login',
+    successFlash: 'You are now logged in!',
     successRedirect: '/',
-    successFlash: 'You are now logged in!'
 });
 
 exports.logout = (req, res) => {
     req.logout();
     req.flash('success', 'You are now logged out!');
     res.redirect('/');
+}
+
+exports.isLoggedIn = (req, res, next) => {
+    // Check if user is authenticated
+    if(req.isAuthenticated()) {
+        next();
+        return;
+    } else {
+        req.flash('error', 'Ooops! You must be logged in to do that!');
+        res.redirect('/login');
+    }
 }
