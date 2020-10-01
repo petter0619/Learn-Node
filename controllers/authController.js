dc4f901f70c913d4373cc91b6655d0c60ca3befa
Petter Carlsson <pettercarlsson@Petters-iMac.local>
2020-10-01 15:19:19 +0200

    Update reset password in MongoDB

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const crypto = require('crypto');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+const promisify = require('es6-promisify');
 
 
 exports.login = passport.authenticate('local', {
@@ -69,5 +70,20 @@ exports.confirmedPasswords = (req, res, next) => {
 }
 
 exports.update = async (req, res) => {
-    
+    const user = await User.findOne({
+        resetPasswordToken: req.params.token,
+        resetPasswordExpires: { $gt: Date.now() }
+    });
+    if(!user) {
+        req.flash('error', 'Password reset is invalid or has expired');
+        return res.redirect('/login');
+    }
+    const setPassword = promisify(user.setPassword, user);
+    await setPassword(req.body.password);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpires = undefined;
+    const updatedUser = await user.save();
+    await req.login(updatedUser);
+    req.flash('success', 'Your password has been reset!');
+    res.redirect('/');
 }
