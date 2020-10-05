8773f6859cae5611c1c482c04153c3dd6d635818
Petter Carlsson <pettercarlsson@Petters-iMac.local>
2020-10-05 14:50:46 +0200

    Created search REST API endpoint

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -105,3 +105,16 @@ exports.getStoresByTag = async (req, res) => {
 
     res.render('tags', { tags: tags, title: 'Tags', tag, stores });
 }
+
+exports.searchStores = async (req, res) => {
+    const stores = await Store.find({
+        $text: {
+            $search: req.query.q
+        }
+    }, {
+        score: { $meta: 'textScore' }
+    }).sort({
+        score: { $meta: 'textScore' }
+    }).limit(5);
+    res.json(stores);
+}
--- routes/index.js
+++ routes/index.js
@@ -63,5 +63,8 @@ router.post('/account/reset/:token',
     catchErrors(authController.update)
 );
 
+/* API Endpoints */
+
+router.get('/api/search', catchErrors(storeController.searchStores));
 
 module.exports = router;
